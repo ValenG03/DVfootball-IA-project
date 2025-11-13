21d2c2ad6748b94c04aefc2b52c1d70a2b7018d2
ValenG03 <valengerold@gmail.com>
2025-11-13 16:00:26 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,6 +3,17 @@ import pandas as pd
 import altair as alt
 from pathlib import Path
 
+col1, col2, col3 = st.columns([1,3,1])
+
+with col1:
+    st.image("boca_logo.png", width=90)
+
+with col2:
+    st.title("⚽ Football & Domestic Violence – AMBA 2024")
+
+with col3:
+    st.image("river_logo.png", width=90)
+
 
 st.set_page_config(
     page_title="Football & Domestic Violence – AMBA 2024",
