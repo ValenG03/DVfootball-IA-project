6788390b9d969172f53e6d8c47571b896e4d255f
ValenG03 <valengerold@gmail.com>
2025-11-18 12:45:11 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -21,7 +21,7 @@ with col2:
         """
         <div style="display:flex; justify-content:center; align-items:center; width:100%;">
             <h1 style="font-size:42px; text-align:center; margin:0; padding:0;">
-                ⚽Football & Domestic Violence in Argentina⚽
+                ⚽ Football & Domestic Violence in Argentina ⚽
             </h1>
         </div>
         """,
@@ -31,6 +31,7 @@ with col2:
 with col3:
     st.markdown("<div style='width:120px; height:1px;'></div>", unsafe_allow_html=True)  # invisible spacer
     st.image("River_logo.png", width=90)
+
 
 # -----------------------------
 # RESEARCH QUESTION + TEAM
@@ -65,6 +66,7 @@ Web page that combines:
 
 and matches **daily call counts** with **Boca & River matches**.
 """)
+
 
 # -----------------------------
 # 1) FILE PATHS – same folder as this script
